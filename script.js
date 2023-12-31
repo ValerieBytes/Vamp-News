e88af646c48d77bc8741a16ae95ade890cade0a3
ValerieBytes <140847810+ValerieBytes@users.noreply.github.com>
2023-12-30 20:23:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function setCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('age-verification').classList.add('show');
+});
+
 function verifyAge() {
     var age = document.getElementById('ageInput').value;
     if (age >= 18) {
@@ -7,14 +21,4 @@ function verifyAge() {
     } else {
         window.location.href = 'under-18.html';
     }
-}
-
-function setCookie(name, value, days) {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = '; expires=' + date.toUTCString();
-    }
-    document.cookie = name + '=' + value + expires + '; path=/';
 }
